d090f9c725eaa0c2a3d3032932ca9722e035f6c7
Safdar Chougle <chouglesafdar22@gmail.com>
2025-07-05 17:26:18 +0530

    Refine footer, sidebar, and layout styles
    Updated Footer for improved styling and punctuation. Sidebar now uses h-screen for consistent height. Removed min-h-screen from layout body to prevent layout issues.

--- portfolio-ui/app/components/Footer.js
+++ portfolio-ui/app/components/Footer.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-black min-w-screen sticky bottom-0 items-center py-8 justify-center text-center text-white">
+    <footer className="bg-black w-screen py-10 text-white flex items-center justify-center text-center">
       <div className="flex justify-center items-center text-center">
         <span className="text-sm sm:text-base lg:text-lg font-mono font-medium">
-          All &copy;2025 rights reserved by Safdar Chougle
+          All &copy;2025 rights reserved by Safdar Chougle.
         </span>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
+
 
 
