8276fe1848f5fb8796e56f6232c536a0604b2571
Safdar Chougle <chouglesafdar22@gmail.com>
2025-07-05 18:05:48 +0530

    Update Footer.js

--- portfolio-ui/app/components/Footer.js
+++ portfolio-ui/app/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-black w-screen h-24 py-10 text-white flex items-center justify-center text-center">
       <div className="flex justify-center items-center text-center">
-        <span className="text-sm flex sm:text-base text-center justify-center items-center lg:text-lg font-mono font-medium">
+        <span className="text-[12px] flex sm:text-sm text-center justify-center items-center lg:text-base font-mono font-medium">
           All &copy;2025 rights reserved by Safdar Chougle.
         </span>
       </div>
