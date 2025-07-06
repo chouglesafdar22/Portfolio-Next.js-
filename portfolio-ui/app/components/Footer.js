240a1c1a690a06a640ecb4326e557d7f7165918f
Safdar Chougle <chouglesafdar22@gmail.com>
2025-07-06 09:46:25 +0530

    Update Footer.js

--- portfolio-ui/app/components/Footer.js
+++ portfolio-ui/app/components/Footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-black w-screen h-24 py-10 text-white flex items-center justify-center text-center">
+    <footer className="bg-black w-screen py-10 text-white flex items-center justify-center text-center">
       <div className="flex justify-center items-center text-center">
         <span className="text-[12px] flex sm:text-sm text-center justify-center items-center lg:text-base font-mono font-medium">
-          All &copy;2025 rights reserved by Safdar Chougle.
+          All &copy;2025 Safdar Chougle rights reserved.
         </span>
       </div>
     </footer>
