9f1da979897f966aa6893e60baacfb343341cffd
Safdar Chougle <chouglesafdar22@gmail.com>
2025-07-20 23:22:59 +0530

    Update Footer.js

--- portfolio-ui/app/components/Footer.js
+++ portfolio-ui/app/components/Footer.js
@@ -8,8 +8,8 @@ function Footer() {
       <ScrollWrapper direction="down" ease="easeOut">
         <footer className="bg-black w-screen relative overflow-hidden px-2 py-5 text-white flex items-center justify-center text-center">
           <div className="flex flex-col gap-3 justify-center items-center text-center">
-            <span className='"text-[5px] flex sm:text-[7px] text-center justify-center items-center lg:text-[9px] font-mono font-light"'>Chiplun, Maharashtra, India</span>
-            <span className="text-sm flex sm:text-base text-center justify-center items-center lg:text-lg font-mono font-medium">
+            <span className='"text-[3px] flex sm:text-[5px] text-center justify-center items-center lg:text-[7px] font-mono font-light"'>Chiplun, Maharashtra, India</span>
+            <span className="text-[12px] flex sm:text-sm text-center justify-center items-center lg:text-base font-mono font-medium">
               &copy; 2025 Safdar Chougle rights reserved.
             </span>
           </div>
