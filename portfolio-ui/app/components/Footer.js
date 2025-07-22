adf3fa83e0ea0a2e59c90c7330d27fb499520a75
Safdar Chougle <chouglesafdar22@gmail.com>
2025-07-22 10:35:06 +0530

    Update Footer.js

--- portfolio-ui/app/components/Footer.js
+++ portfolio-ui/app/components/Footer.js
@@ -5,7 +5,7 @@ import ScrollWrapper from './ScrollWrapper';
 function Footer() {
   return (
     <div>
-      <ScrollWrapper direction="down" ease="easeOut">
+      <ScrollWrapper direction="down" ease="fade">
         <footer className="bg-black w-screen relative overflow-hidden px-2 py-5 text-white flex items-center justify-center text-center">
           <div className="flex flex-col gap-2.5 justify-center items-center text-center">
             <p className="text-[12px] flex sm:text-sm text-center justify-center items-center lg:text-base font-light font-mono">Chiplun, Maharashtra, India</p>
