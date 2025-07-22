494860bb71a732138e1e60b34382b0adabfaa201
Safdar Chougle <chouglesafdar22@gmail.com>
2025-07-22 18:18:10 +0530

    Update Footer.js

--- portfolio-ui/app/components/Footer.js
+++ portfolio-ui/app/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <motion.footer initial={{ opaity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8, delay: 0, ease: "easeInOut" }} className="bg-black w-screen relative overflow-hidden px-2 py-5 text-white flex items-center justify-center text-center">
       <div className="flex flex-col gap-2.5 justify-center items-center text-center">
         <p className="text-[12px] flex sm:text-sm text-center justify-center items-center lg:text-base font-light font-mono">Chiplun, Maharashtra, India</p>
-        <span className="text-sm flex sm:text-base text-center justify-center items-center lg:text-lg font-mono font-medium">
+        <span className="text-[13px] flex sm:text-[15px] text-center justify-center items-center lg:text-[17px] font-mono font-medium">
           &copy; 2025 Safdar Chougle rights reserved.
         </span>
       </div>
