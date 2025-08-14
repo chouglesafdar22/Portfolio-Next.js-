46dc1d8f88b55ba95c2a01817728093e42f5694f
Safdar Chougle <chouglesafdar22@gmail.com>
2025-08-14 12:45:55 +0530

    Update page.js

--- portfolio-ui/app/page.js
+++ portfolio-ui/app/page.js
@@ -68,7 +68,7 @@ export default function Home() {
           <div className="container mx-auto flex px-10 pb-10 sm:pt-20 pt-[70px] text-center items-center justify-center flex-col">
             <Image src="/images/my-image.jpg" alt="my-profile" width={230} height={230} className="lg:w-2/5 md:w-2/5 mb-10 object-cover object-center rounded-full" />
             <div className="text-center lg:w-2/3 w-full">
-              <h1 className="title-font sm:text-4xl lg:text-5xl text-3xl mb-4 font-bold font-mono text-white">Safdar Chougle</h1>
+              <h1 className="title-font sm:text-5xl lg:text-6xl text-4xl mb-4 font-bold font-mono text-white">Safdar Chougle</h1>
               <p className="leading-relaxed text-lg sm:text-xl lg:text-2xl mb-8 font-light font-mono">FrontEnd Developer - Next.js, React.js, Tailwind CSS</p>
               <div className="flex sm:flex-row flex-col justify-center gap-4">
                 <Button icon={<IoNewspaperOutline />} onClick={openResume} title="Resume" hoverTextSize="hover:text-xl" textSize="text-lg" size={40} bgColor="bg-indigo-500" hoverTextColor="hover:text-gray-300" textColor="text-white" hoverSize="hover:w-44" hoverBgColor="hover:bg-gray-600" />
@@ -85,7 +85,7 @@ export default function Home() {
       <ScrollWrapper direction="down" ease="easeOut">
         <section id="about" className="text-gray-400">
           <div className="container size mx-auto flex flex-col justify-center items-center py-10 px-10 gap-8">
-            <h2 className="lg:text-4xl sm:text-3xl text-2xl text-white font-mono font-medium">About</h2>
+            <h2 className="lg:text-5xl sm:text-4xl text-3xl text-white font-mono font-medium">About</h2>
             <ScrollWrapper direction="right" ease="easeInOut">
               <p className="lg:text-2xl sm:text-xl text-lg font-mono font-normal lg:px-16">
                 Hii, I’m Safdar Chougle, a final-year Bachelor of Computer Science student at the University of Mumbai. I specialize in frontend development and have hands-on experience with Next.js, React.js, and Tailwind CSS. I enjoy crafting responsive, clean, and user-friendly web applications that deliver great user experiences.
@@ -104,7 +104,7 @@ export default function Home() {
       <ScrollWrapper direction="up" ease="easeIn">
         <section id="projects">
           <div className=" w-full flex flex-col justify-center items-center py-10 px-10 gap-8">
-            <h2 className="lg:text-4xl sm:text-3xl text-2xl text-white font-mono font-medium">Projects</h2>
+            <h2 className="lg:text-5xl sm:text-4xl text-3xl text-white font-mono font-medium">Projects</h2>
             <div className="flex items-center xl:justify-center sm:overflow-x-auto w-full sm:flex-row flex-col gap-6">
               <ScrollWrapper direction="down" ease="easeOut">
                 <ProjectCard onClick={moveToHome} image="/images/portfolio-nextjs.png" title="Portfolio-Next.js" description="I have built a fully responsive portfolio UI using Next.js to showcase my projects — both completed and upcoming. All buttons and cards are fully functional, and the UI is designed to work smoothly across desktop, tablet, and mobile devices." />
@@ -124,7 +124,7 @@ export default function Home() {
       <ScrollWrapper direction="up" ease="easeIn">
         <section id="skills" className="text-gray-400">
           <div className="container mx-auto flex flex-col justify-center items-center py-10 px-10 gap-8">
-            <h2 className="lg:text-4xl sm:text-3xl text-2xl text-white font-mono font-medium">Skills</h2>
+            <h2 className="lg:text-5xl sm:text-4xl text-3xl text-white font-mono font-medium">Skills</h2>
             <ScrollWrapper direction="left" ease="easeInOut">
               <div className="flex flex-wrap gap-14 items-center text-center justify-center ">
                 {skillIcons.map((item) => (
@@ -145,7 +145,7 @@ export default function Home() {
       <ScrollWrapper direction="down" ease="easeOut">
         <section id="contact" className="text-gray-400">
           <div className="container mx-auto flex flex-col justify-center items-center py-10 px-10 gap-8">
-            <h2 className="lg:text-4xl sm:text-3xl text-2xl text-white font-mono font-medium">Contact</h2>
+            <h2 className="lg:text-5xl sm:text-4xl text-3xl text-white font-mono font-medium">Contact</h2>
             <div className="flex justify-center items-center text-center">
               <ContactForm />
             </div>
