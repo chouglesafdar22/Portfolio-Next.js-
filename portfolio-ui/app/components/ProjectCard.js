3595a45a8c454077cbc65f086ee1264a82c2021a
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-09 11:09:28 +0530

    Center content in ProjectCard component
    Added 'items-center' to the ProjectCard container's class list to ensure child elements are horizontally centered within the card.

--- portfolio-ui/app/components/ProjectCard.js
+++ portfolio-ui/app/components/ProjectCard.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function ProjectCard({ image, title, description, onClick }) {
     return (
-        <div onClick={onClick} className='flex flex-col cursor-pointer text-white hover:text-gray-300 hover:bg-gray-600 transition-all duration-300 px-2 py-1 m-2 w-72 h-80 sm:w-80 sm:h-96 gap-5 ease-in-out rounded-lg shadow-lg shadow-indigo-800 hover:shadow-xl hover:shadow-gray-900 bg-indigo-500'>
+        <div onClick={onClick} className='flex flex-col cursor-pointer text-white hover:text-gray-300 hover:bg-gray-600 transition-all duration-300 items-center px-2 py-1 m-2 w-72 h-80 sm:w-80 sm:h-96 gap-5 ease-in-out rounded-lg shadow-lg shadow-indigo-800 hover:shadow-xl hover:shadow-gray-900 bg-indigo-500'>
             <Image src={image} className='object-contain rounded-md sm:w-80 sm:h-48' alt={title} width={256} height={180} />
             <h3 className='lg:text-2xl sm:text-xl text-lg font-normal font-mono'>{title}</h3>
             <p className='lg:text-xl h-full sm:text-lg max-h-24 w-full break-words overflow-y-auto text-md font-light font-mono'>{description}</p>
