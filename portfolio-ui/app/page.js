69fef7fc91a8e6bcf02fbbdde8784d0923be5234
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-09 12:51:33 +0530

    Add Resume button to homepage
    Uncomments and adds the Resume button to the homepage's social links section, allowing users to access the resume directly from the main page.

--- portfolio-ui/app/page.js
+++ portfolio-ui/app/page.js
@@ -77,10 +77,10 @@ export default function Home() {
               <h1 className="title-font sm:text-4xl lg:text-5xl text-3xl mb-4 font-bold font-mono text-white">Safdar Chougle</h1>
               <p className="leading-relaxed text-lg sm:text-xl lg:text-2xl mb-8 font-light font-mono">Full-Stack Web Developer — Next.js, Mern Stack, TypeScript, Tailwind CSS</p>
               <div className="flex sm:flex-row flex-col justify-center gap-4">
-                {/* <Button icon={<IoNewspaperOutline />} onClick={openResume} title="Resume" hoverTextSize="hover:text-xl" textSize="text-lg" size={40} bgColor="bg-indigo-500" hoverTextColor="hover:text-gray-300" textColor="text-white" hoverSize="hover:w-44" hoverBgColor="hover:bg-gray-600" /> */}
                 <Button icon={<FaGithub />} onClick={openGitHub} title="GitHub" hoverTextSize="hover:text-xl" textSize="text-lg" size={40} hoverSize="hover:w-44" bgColor="bg-indigo-500" textColor="text-white" hoverTextColor="hover:text-gray-300" hoverBgColor="hover:bg-gray-600" />
                 <Button icon={<FaLinkedin />} onClick={openLinkedIn} title="LinkedIn" hoverTextSize="hover:text-xl" textSize="text-lg" size={40} hoverSize="hover:w-44" bgColor="bg-indigo-500" textColor="text-white" hoverTextColor="hover:text-gray-300" hoverBgColor="hover:bg-gray-600" />
                 <Button icon={<TbBrandLinktree />} onClick={openLinkTree} size={40} bgColor="bg-indigo-500" title="LinkTree" textColor="text-white" hoverBgColor="hover:bg-gray-600" hoverSize="hover:w-44" hoverTextColor="hover:text-gray-300" textSize="text-lg" hoverTextSize="hover:text-xl" />
+                <Button icon={<IoNewspaperOutline />} onClick={openResume} title="Resume" hoverTextSize="hover:text-xl" textSize="text-lg" size={40} bgColor="bg-indigo-500" hoverTextColor="hover:text-gray-300" textColor="text-white" hoverSize="hover:w-44" hoverBgColor="hover:bg-gray-600" />
               </div>
             </div>
           </div>
