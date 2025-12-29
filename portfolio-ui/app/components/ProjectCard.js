381d733dbba2f0d54de219eb3fa183b26a64900f
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-29 22:09:36 +0530

    Update ProjectCard.js
    changing size of projectcard

--- portfolio-ui/app/components/ProjectCard.js
+++ portfolio-ui/app/components/ProjectCard.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 function ProjectCard({ image, title, description, onClick }) {
     return (
-        <div onClick={onClick} className='flex flex-col cursor-pointer text-white hover:text-gray-300 hover:bg-gray-600 transition-all duration-300 items-center px-2.5 py-2.5 m-2 w-72 h-80 sm:w-80 sm:h-96 gap-5 ease-in-out rounded-lg shadow-lg shadow-indigo-800 hover:shadow-xl hover:shadow-gray-900 bg-indigo-500'>
-            <Image src={image} className='object-contain rounded-md sm:w-80 sm:h-48' alt={title} width={256} height={180} />
+        <div onClick={onClick} className='flex flex-col cursor-pointer text-white hover:text-gray-300 hover:bg-gray-600 transition-all duration-300 items-center px-2.5 py-2.5 m-2 h-80 w-80 gap-5 ease-in-out rounded-lg shadow-lg shadow-indigo-800 hover:shadow-xl hover:shadow-gray-900 bg-indigo-500'>
+            <Image src={image} className='object-contain rounded-md' alt={title} width={300} height={300} />
             <h3 className='lg:text-2xl sm:text-xl text-lg font-normal font-mono'>{title}</h3>
             <p className='lg:text-xl h-full sm:text-lg max-h-24 w-full break-words overflow-y-auto text-md font-light font-mono'>{description}</p>
         </div>
