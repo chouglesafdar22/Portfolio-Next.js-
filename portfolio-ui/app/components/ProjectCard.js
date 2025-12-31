550c9f9d9d35bffd90d3deae79ce0edf723a1d50
Safdar Chougle <chouglesafdar22@gmail.com>
2025-12-31 21:37:10 +0530

    Adjust ProjectCard width to 310px
    Changed the width of the ProjectCard component from 315px to 310px for improved layout consistency.

--- portfolio-ui/app/components/ProjectCard.js
+++ portfolio-ui/app/components/ProjectCard.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function ProjectCard({ image, title, description, onClick }) {
     return (
-        <div onClick={onClick} className='flex flex-col cursor-pointer text-white hover:text-gray-300 hover:bg-gray-600 transition-all duration-300 items-center px-2.5 py-2.5 m-2 h-80 w-[315px] gap-5 ease-in-out rounded-lg shadow-lg shadow-indigo-800 hover:shadow-xl hover:shadow-gray-900 bg-indigo-500'>
+        <div onClick={onClick} className='flex flex-col cursor-pointer text-white hover:text-gray-300 hover:bg-gray-600 transition-all duration-300 items-center px-2.5 py-2.5 m-2 h-80 w-[310px] gap-5 ease-in-out rounded-lg shadow-lg shadow-indigo-800 hover:shadow-xl hover:shadow-gray-900 bg-indigo-500'>
             <Image src={image} className='object-contain rounded-md' alt={title} width={300} height={300} />
             <h3 className='lg:text-2xl sm:text-xl text-lg font-normal font-mono'>{title}</h3>
             <p className='lg:text-xl h-full sm:text-lg max-h-24 w-full break-words overflow-y-auto text-md font-light font-mono'>{description}</p>
