ed80427e8520af7b3260b220013bec04720dfe27
Safdar Chougle <chouglesafdar22@gmail.com>
2026-01-01 00:02:41 +0530

    Update footer to display current year dynamically
    Replaces the hardcoded year in the footer with a dynamic value using JavaScript's Date API, ensuring the displayed year is always current.

--- portfolio-ui/app/components/Footer.js
+++ portfolio-ui/app/components/Footer.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { motion } from 'framer-motion'
 
 function Footer() {
+const currentYear = new Date().getFullYear();
+
   return (
     <motion.footer initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8, delay: 0, ease: "easeInOut" }} className="bg-black w-screen relative bottom overflow-hidden px-3 py-7 text-white flex items-center justify-center text-center">
       <div className="flex flex-col gap-2.5 justify-center items-center text-center">
         <p className="text-[12px] flex sm:text-sm text-center justify-center items-center lg:text-base font-light font-mono">Chiplun, Maharashtra, India</p>
         <span className="text-[13px] flex sm:text-[15px] text-center justify-center items-center lg:text-[17px] font-mono font-medium">
-          &copy;2025 rights reserved.
+          &copy;{currentYear} rights reserved.
         </span>
       </div>
     </motion.footer>
